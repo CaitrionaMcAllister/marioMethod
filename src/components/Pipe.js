032aae71943f7c30e5e9054cbd222948b251aba5
Caitríona McAllister <91537940+CaitrionaMcAllister@users.noreply.github.com>
2023-04-20 17:02:28 +0100

    test14

--- src/components/Pipe.js
+++ src/components/Pipe.js
@@ -2,14 +2,16 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function Pipe() {
-  const model = useLoader(GLTFLoader, "./pipe.glb");
+  const pipe1 = useLoader(GLTFLoader, "./pipe.glb");
+  const pipe2 = useLoader(GLTFLoader, "./pipe.glb");
+
   // console.log(model);
   return (
     <primitive
-      object={model.scene}
+      object={pipe1.scene}
       scale={3}
       rotation={[124, 0, 0]}
-      position={[3, -22, -1]}
+      position={[3, -22, -1.5]}
     />
   );
 }
