25c817c2cfecc10b57e63c3c3b62c014b60ce029
Caitríona McAllister <91537940+CaitrionaMcAllister@users.noreply.github.com>
2023-04-20 17:35:20 +0100

    test14

--- src/components/Pipe.js
+++ src/components/Pipe.js
@@ -17,3 +17,21 @@ function Pipe() {
 }
 
 export { Pipe };
+
+// function Pipes(props) {
+//   const { nodes, materials } = useGLTF("/eye/scene.gltf");
+//   return (
+//     <group
+//       {...props}
+//       dispose={null}
+//       rotation={[Math.PI, 0, -Math.PI / 2]}
+//       scale={[1, 1, 1]}
+//     >
+//       <mesh
+//         geometry={nodes.Sphere001_Eye_0.geometry}
+//         material={materials.material}
+//       />
+//     </group>
+//   );
+// }
+// useGLTF.preload("/eye/scene.gltf");
