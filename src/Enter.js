ee4ee9987e00b40c0d88e8c13cb0d94ada85903d
Caitríona McAllister <91537940+CaitrionaMcAllister@users.noreply.github.com>
2023-04-25 00:16:11 +0100

    test18

--- src/Enter.js
+++ src/Enter.js
@@ -1,6 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { OrbitControls } from "@react-three/drei";
+import {
+  OrbitControls,
+  AccumulativeShadows,
+  RandomizedLight,
+  Environment,
+} from "@react-three/drei";
 import "./style2.css";
 import { Arcade } from "./components/Arcade";
 
@@ -16,17 +21,33 @@ export default function EnterPage() {
         position: [-4, 3, 6],
       }}
     >
+      <color attach="background" args={["hotpink"]} />
+      <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/industrial_workshop_foundry_1k.hdr" />
       <ambientLight color="pink" intensity={1} />
-
       <OrbitControls makeDefault />
+
       <mesh
         receiveShadow
         position-y={-2}
         rotation-x={-Math.PI * 0.5}
         scale={10}
       >
-        <planeGeometry />
-        <meshStandardMaterial color={"red"} />
+        <AccumulativeShadows
+          temporal
+          frames={100}
+          scale={20}
+          alphaTest={0.85}
+          color="hotpink"
+          colorBlend={2}
+        >
+          <RandomizedLight
+            amount={8}
+            radius={5}
+            ambient={0.5}
+            position={[5, 5, -10]}
+            bias={0.001}
+          />
+        </AccumulativeShadows>
       </mesh>
 
       <Arcade />
